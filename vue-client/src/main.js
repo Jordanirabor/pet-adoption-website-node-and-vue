51397b56974dc15e9d9b61ed3d8e7ee15e3afbfe
Jordan <iraborjordan1@gmail.com>
2020-01-19 21:31:02 +0100

    rearranged imports

--- vue-client/src/main.js
+++ vue-client/src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue';
+import Buefy from 'buefy';
 import App from './App.vue';
 import routes from './routes';
 import VueRouter from 'vue-router'
-import Buefy from 'buefy';
 import 'buefy/dist/buefy.css';
 
 Vue.use(Buefy);
